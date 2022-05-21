8bab12137032ce29f8f5f3f8a7e6d1059a5e5b9d
pauliancu97 <paul97iancu@gmail.com>
2022-05-21 19:13:52 +0300

    added wind direction, weather icon

--- rust-weather-cli/src/main.rs
+++ rust-weather-cli/src/main.rs
@@ -3,7 +3,9 @@ mod weather;
 
 use std::error::Error;
 use std::io;
+use std::collections::HashMap;
 use crossterm::terminal::enable_raw_mode;
+use lazy_static::lazy_static;
 use tui::{Terminal, Frame};
 use tui::backend::{CrosstermBackend, Backend};
 use tui::layout::{Layout, Constraint};
@@ -22,15 +24,8 @@ struct WeatherMainStatsUi {
     feels_like: String,
     wind: String,
     visibility: String,
-    humidity: String
-}
-
-async fn test() -> Option<()> {
-    let coordinates = get_coordinates().await?;
-    println!("{:?}", coordinates);
-    let weather = get_weather(coordinates.lat, coordinates.lon).await?;
-    println!("{:?}", weather);
-    Some(())
+    humidity: String,
+    icon: Vec<String>
 }
 
 async fn get_weather_current_location() -> Option<WeatherResponse> {
@@ -39,22 +34,157 @@ async fn get_weather_current_location() -> Option<WeatherResponse> {
     Some(response)
 }
 
+fn get_wind_direction(degree: f64) -> String {
+    let wind_directions = ["↓", "↙", "←", "↖", "↑", "↗", "→", "↘"];
+    let degree = degree + 22.5;
+    let degree = if degree < 0.0 {
+        360.0 + (degree % 360.0)
+    } else {
+        degree % 360.0
+    };
+    let index = ((degree as u64) / 45) as usize;
+    String::from(wind_directions[index])
+}
+
 fn get_weather_ui(weather_response: &WeatherResponse) -> Option<WeatherMainStatsUi> {
     let weather_desc = weather_response.weather.first()?.description.clone();
+    let weather_icon = weather_response.weather.first()?.icon.clone();
     let description = format!("Description: {}", weather_desc);
     let temperatures = format!("Temperatures: {:.1} - {:.1} °C", weather_response.main.temp_min, weather_response.main.temp_min);
     let feels_like = format!("Feels like: {:.1} °C", weather_response.main.feels_like);
-    let wind = format!("Wind: {:.1} km/h", weather_response.wind.speed);
+    let wind_direction = get_wind_direction(weather_response.wind.deg);
+    let wind = format!("Wind: {} {:.1} km/h", wind_direction, weather_response.wind.speed);
     let visibility = format!("Visibility: {:.1} km", weather_response.visibility);
     let humidity = format!("Humidity: {:.1} %", weather_response.main.humidity);
+    let icon = get_weather_icon(&weather_icon)?;
     Some(WeatherMainStatsUi {
         description,
         temperatures,
         feels_like,
         wind,
         visibility,
-        humidity
+        humidity,
+        icon
     })
+}
+
+fn get_weather_icon(icon_id: &str) -> Option<Vec<String>> {
+    lazy_static! {
+        static ref ICON_MAP: HashMap<u32, Vec<String>> = {
+            let mut map = HashMap::new();
+            map.insert(1u32,
+                [
+                    "\033[38;5;226m    \\   /    \033[0m",
+			        "\033[38;5;226m     .-.     \033[0m",
+			        "\033[38;5;226m  ‒ (   ) ‒  \033[0m",
+			        "\033[38;5;226m     `-᾿     \033[0m",
+			        "\033[38;5;226m    /   \\    \033[0m",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map.insert(2u32,
+                [
+                    "\033[38;5;226m   \\  /\033[0m      ",
+			        "\033[38;5;226m _ /\"\"\033[38;5;250m.-.    \033[0m",
+			        "\033[38;5;226m   \\_\033[38;5;250m(   ).  \033[0m",
+			        "\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m",
+			        "             ",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map.insert(3u32,
+                [
+                    "             ",
+			        "\033[38;5;250m     .--.    \033[0m",
+			        "\033[38;5;250m  .-(    ).  \033[0m",
+			        "\033[38;5;250m (___.__)__) \033[0m",
+                    "             ",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map.insert(4u32,
+                [
+                    "             ",
+			        "\x1b[38;5;240;1m     .--.    \x1b[0m",
+			        "\x1b[38;5;240;1m  .-(    ).  \x1b[0m",
+			        "\x1b[38;5;240;1m (___.__)__) \x1b[0m",
+			        "             ",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map.insert(9u32,
+                [
+                    "\033[38;5;226m _`/\"\"\033[38;5;250m.-.    \033[0m",
+			        "\033[38;5;226m  ,\\_\033[38;5;250m(   ).  \033[0m",
+			        "\033[38;5;226m   /\033[38;5;250m(___(__) \033[0m",
+			        "\033[38;5;111m     ʻ ʻ ʻ ʻ \033[0m",
+			        "\033[38;5;111m    ʻ ʻ ʻ ʻ  \033[0m",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map.insert(10u32,
+                [
+                    "\033[38;5;240;1m     .-.     \033[0m",
+			        "\033[38;5;240;1m    (   ).   \033[0m",
+			        "\033[38;5;240;1m   (___(__)  \033[0m",
+			        "\033[38;5;21;1m  ‚ʻ‚ʻ‚ʻ‚ʻ   \033[0m",
+			        "\033[38;5;21;1m  ‚ʻ‚ʻ‚ʻ‚ʻ   \033[0m",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map.insert(11u32,
+                [
+                    "\033[38;5;240;1m     .-.     \033[0m",
+                    "\033[38;5;240;1m    (   ).   \033[0m",
+			        "\033[38;5;240;1m   (___(__)  \033[0m",
+			        "\033[38;5;21;1m  ‚ʻ\033[38;5;228;5m⚡\033[38;5;21;25mʻ‚\033[38;5;228;5m⚡\033[38;5;21;25m‚ʻ   \033[0m",
+			        "\033[38;5;21;1m  ‚ʻ‚ʻ\033[38;5;228;5m⚡\033[38;5;21;25mʻ‚ʻ   \033[0m",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map.insert(13u32,
+                [
+                    "\033[38;5;240;1m     .-.     \033[0m",
+			        "\033[38;5;240;1m    (   ).   \033[0m",
+			        "\033[38;5;240;1m   (___(__)  \033[0m",
+			        "\033[38;5;255;1m   * * * *   \033[0m",
+			        "\033[38;5;255;1m  * * * *    \033[0m",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map.insert(50u32,
+                [
+                    "             ",
+			        "\033[38;5;251m _ - _ - _ - \033[0m",
+			        "\033[38;5;251m  _ - _ - _  \033[0m",
+			        "\033[38;5;251m _ - _ - _ - \033[0m",
+                    "             ",
+                ]
+                    .into_iter()
+                    .map(|string| String::from(string))
+                    .collect()
+            );
+            map
+        };
+    }
+    let id = icon_id[..2].parse::<u32>().ok()?;
+    ICON_MAP.get(&id).map(|strings| strings.clone())
 }
 
 fn ui<B: Backend>(rect: &mut Frame<B>, weather_response: &WeatherMainStatsUi) {
@@ -64,13 +194,25 @@ fn ui<B: Backend>(rect: &mut Frame<B>, weather_response: &WeatherMainStatsUi) {
             .margin(0)
             .constraints(
                 [
+                    Constraint::Percentage(40),
                     Constraint::Percentage(30),
                     Constraint::Percentage(30)
                 ].as_ref()
             )
             .split(
-                tui::layout::Rect { x: size.x, y: size.y, width: size.width, height: 5 }
+                tui::layout::Rect { x: size.x, y: size.y, width: size.width, height: 7 }
             );
+        let spans: Vec<_> = weather_response.icon.iter()
+            .map(|string| Spans::from(vec![Span::raw(string)]))
+            .collect();
+        let paragraph = Paragraph::new(spans)
+            .block(
+                Block::default()
+                    .borders(Borders::ALL)
+                    .style(Style::default().fg(tui::style::Color::White))
+                    .border_type(tui::widgets::BorderType::Plain)
+            );
+        rect.render_widget(paragraph, chunks[0]);
         let spans = vec![
             Spans::from(vec![Span::raw(&weather_response.description)]),
             Spans::from(vec![Span::raw(&weather_response.temperatures)]),
@@ -83,7 +225,7 @@ fn ui<B: Backend>(rect: &mut Frame<B>, weather_response: &WeatherMainStatsUi) {
                     .style(Style::default().fg(tui::style::Color::White))
                     .border_type(tui::widgets::BorderType::Plain)
             );
-        rect.render_widget(paragraph, chunks[0]);
+        rect.render_widget(paragraph, chunks[1]);
         let spans = vec![
             Spans::from(vec![Span::raw(&weather_response.wind)]),
             Spans::from(vec![Span::raw(&weather_response.visibility)]),
@@ -96,7 +238,7 @@ fn ui<B: Backend>(rect: &mut Frame<B>, weather_response: &WeatherMainStatsUi) {
                     .style(Style::default().fg(tui::style::Color::White))
                     .border_type(tui::widgets::BorderType::Plain)
             );
-        rect.render_widget(paragraph, chunks[1]);
+        rect.render_widget(paragraph, chunks[2]);
 }
 
 #[tokio::main]
